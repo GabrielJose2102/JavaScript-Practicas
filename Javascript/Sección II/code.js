176128a5f60ff35e05e7357b18d210da74e88ca1
Gabriel <gabrieltorrealba09@gmail.com>
2024-07-07 11:11:48 -0430

    Agregado ejercicio de calculadora

--- Javascript/Sección II/code.js
+++ Javascript/Sección II/code.js
@@ -24,26 +24,32 @@ do {
 
     portero(nombre, edad, hora);
 
-} while (hora <= 5);
-
- */
+} while (hora <= 5); */
 
 
 
-let dias = 2;
-let asistio
 
+let alumnos = prompt('¿Cuantos alumnos hay inscritos en la materia?')
+let asistio;
 let listado = {
-    nombres: ['Gabriel Torrealba', 'Elvis Torrealba', 'Wilkerman Torrealba'],
-    asistencias: [0,0,0],
-    inasistencias: [0,0,0]
+    nombres: [],
+    asistencias: [],
+    inasistencias: []
 } 
 
+const crearLista = ()=>{
+    for (let alumno = 0; alumno < alumnos; alumno++) {
+        listado.nombres[alumno] = prompt(`Cual es el Nombre del alumno Nro ${alumno+1}`);
+        listado.asistencias[alumno] = 0;
+        listado.inasistencias[alumno] = 0;
+    }
+}
+
 const tomarAsistencia = ()=>{
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 3; i++) {
         for (const lista in listado.nombres) {
 
-            asistio = prompt(`Dia: ${i} Alumno ${listado.nombres[lista]} ¿Vino?`);
+            asistio = prompt(`Dia: ${i+1} Alumno ${listado.nombres[lista]} ¿Vino?`);
        
            if (asistio == 'si') {
                listado.asistencias[lista] = ++listado.asistencias[lista];
@@ -59,15 +65,55 @@ const tomarAsistencia = ()=>{
 const imprimirHoja = ()=>{
     for (const lista in listado.nombres) {
 
-        document.write(`${lista}. ${listado.nombres[lista]}: <br>`);
+        document.write(`Nro ${parseInt(lista)+1}. ${listado.nombres[lista]}: <br>`);
 
         if (listado.inasistencias[lista] >= 3) {
-            document.write(`Posees ${listado.inasistencias[lista]} inasistencias has reprobado la materia <br><br>`);
+            document.write(`Posees ${listado.asistencias[lista]} asistencias <br>
+                                   ${listado.inasistencias[lista]} inasistencias has
+                                   reprobado la materia por superar el 9% <br><br>`);
         } else {
-            document.write(`Posees ${listado.asistencias[lista]} asistencias has aprobado la materia <br><br>`);
+            document.write(`Posees ${listado.inasistencias[lista]} inasistencias <br>
+                                   ${listado.asistencias[lista]} asistencias has aprobado la materia <br><br>`);
         }
     }
 }
 
+crearLista();
 tomarAsistencia();
 imprimirHoja();
+
+
+
+
+
+const calculadora = (opc, num1, num2)=>{
+    if (opc == 1) {
+        return num1 + num2;
+    } else if (opc == 2) {
+        return num1 - num2;
+    } else if (opc == 3) {
+        return num1 * num2;
+    } else if (opc == 4) {
+        return num1 / num2;
+    } else{
+        console.log('Opcion no valido');
+    }
+}
+
+
+let opcion= 0;
+
+do {
+     opcion = parseInt(prompt(`
+                    1.Suma
+                     2.Resta
+                     3.Multiplicación
+                     4.División
+                     5.Salir de la app`));
+
+    let num1 = parseInt(prompt('Ingresa el primer numero'));
+    let num2 = parseInt(prompt('Ingresa el primer numero'));
+
+console.log(calculadora(opcion, num1, num2));
+
+} while (opcion<5 && opcion>0);
